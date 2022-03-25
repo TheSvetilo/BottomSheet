aa0a13c755832bafde843177b8d07043221ec37b
TheSvetilo <svet2315@gmail.com>
2022-03-25 21:17:44 +0300

    added: - localization (en-ru) - MyClickableSpan.kt class

--- app/src/main/java/com/example/bottom/MainActivity.kt
+++ app/src/main/java/com/example/bottom/MainActivity.kt
@@ -1,9 +1,18 @@
 package com.example.bottom
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.TextPaint
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
@@ -27,6 +36,40 @@ class MainActivity : AppCompatActivity() {
             dialog.setCancelable(false)
             dialog.setContentView(view)
             dialog.show()
+        }
+
+        val agreementTextView = findViewById<TextView>(R.id.agreementText)
+        val fullText = getString(R.string.agreement_full_text)
+        val agreement = getString(R.string.confidential_info)
+        val policy = getString(R.string.privacy_policy)
+
+        val spannableString = SpannableString(fullText)
+
+        val agreementClickable = MyClickableSpan {
+            Snackbar.make(it, "Agreement here", Snackbar.LENGTH_SHORT).show()
+        }
+
+        val policyClickable = MyClickableSpan {
+            Snackbar.make(it, "Policy here", Snackbar.LENGTH_SHORT).show()
+        }
+
+        spannableString.setSpan(
+            agreementClickable,
+            fullText.indexOf(agreement),
+            fullText.indexOf(agreement) + agreement.length,
+            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        spannableString.setSpan(
+            policyClickable,
+            fullText.indexOf(policy),
+            fullText.indexOf(policy) + policy.length,
+            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+
+        agreementTextView.run {
+            text = spannableString
+            movementMethod = LinkMovementMethod.getInstance()
+            highlightColor = Color.TRANSPARENT
         }
     }
 }
